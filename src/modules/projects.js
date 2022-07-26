146ceadf0abf5935afc464ad446a3be986fe3e1d
DarkWool <cooper27@live.com.mx>
2022-07-25 19:59:36 -0500

    Added a module to create project objects

---
+++ src/modules/projects.js
@@ -0,0 +1,19 @@
+const projects = [];
+const homeProject = project("Home");
+
+function project(title) {
+    const tasks = [];
+    const addTask = (task) => tasks.push(task) - 1;
+    const deleteTask = (taskIndex) => tasks.splice(taskIndex, 1);
+
+    return {
+        title,
+        tasks,
+        addTask,
+        deleteTask
+    }
+}
+
+export {
+    homeProject
+};
