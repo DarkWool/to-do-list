9c5769bee56f6651207f24b85b719a094a6f6253
DarkWool <cooper27@live.com.mx>
2022-09-01 13:40:32 -0500

    Modified the task object structure
    Added a function to tasks objects to change their 'completed' state

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -1,33 +1,21 @@
-import { projects } from "./projects";
-
 // Factory function to create new tasks
-function task(title, details, date, priority, completed = false) {
+function task(title, details, date, priority) {    
+    let completed = false;
+
     return {
         title,
         details,
         date,
         priority,
-        getInfo: () => console.log(`${title} - ${details} - ${date} - ${priority} - ${completed}`),
+        toggleCompletedState() {
+            completed = (completed) ? false : true;
+        },
         get completed() {
             return completed;
         },
-        set completed(value) {
-            if (value === true || value === false) {
-                completed = value;
-            }
-        }
-    }
-}
-
-function markTaskCompleted(activeTab, index) {
-    if (projects[activeTab].tasks[index].completed === true) {
-        projects[activeTab].tasks[index].completed = false;
-    } else {
-        projects[activeTab].tasks[index].completed = true;
     }
 }
 
 export {
-    task,
-    markTaskCompleted
+    task
 };
